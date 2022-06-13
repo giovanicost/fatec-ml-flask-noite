ae8631f8b53f029c1c5d0d79f867b899237958a0
giovanicost <giovani1111costalima@gmail.com>
2022-06-13 22:22:32 +0000

    Versão Final

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html', nome='Fulano')
+  return render_template('index.html', nome='Credit Card Lead Prediction')
 
 @app.route('/predicao', methods=['POST'])
 def predicao():
-  comentario = request.form['comentario']
-  predicao = model.predict([comentario])
+  Age = request.form['Age']
+  Vintage = request.form['Vintage']
+  Credit_Product = request.form['Credit_Product']
+
+  array=[[str(Age), str(Vintage), str(Credit_Product)]]
+
+  predicao = model.predict(array)
+
   return render_template('resposta.html', predicao=predicao[0])
 
 app.run(debug=True)
